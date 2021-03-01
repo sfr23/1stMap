0436694f98b10a48bb4fdd0597d0bb621f1b4724
sfr23 <46600928+sfr23@users.noreply.github.com>
2021-03-01 23:01:17 +0100

    sasd

---
+++ full-interior2258.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="full-interior2258" tilewidth="32" tileheight="32" tilecount="8592" columns="16">
+ <image source="Interiors_32x32.png" width="512" height="17184"/>
+</tileset>
