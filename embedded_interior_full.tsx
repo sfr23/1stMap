fa1d6d13c396fccbbb7c31e8f7a1526884c55d2e
sfr23 <46600928+sfr23@users.noreply.github.com>
2021-03-02 12:36:25 +0100

    cleanup

--- embedded_interior_full.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="embedded_interior_full" tilewidth="32" tileheight="32" tilecount="8592" columns="16">
- <image source="../../../Downloads/Modern_Interiors/1_Interiors/32x32/Interiors_32x32.png" width="512" height="17184"/>
-</tileset>
