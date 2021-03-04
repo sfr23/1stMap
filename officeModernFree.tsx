5d0824d22555213e77edad9c02b28ed7676df358
sfr23 <46600928+sfr23@users.noreply.github.com>
2021-03-04 12:39:46 +0100

    Cleanup

--- officeModernFree.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="officeModernFree" tilewidth="32" tileheight="32" spacing="1" tilecount="810" columns="15">
- <image source="../../../Downloads/Modern tiles_Free/Interiors_free/32x32/Interiors_free_32x32.png" width="512" height="1792"/>
-</tileset>
